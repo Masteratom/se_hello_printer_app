de0b7565aa5a54a3626097065b2b6a24f196bff0
Masteratom <Masteratom@siemka.pl>
2018-04-07 12:02:08 +0200

    heroku 2

--- test/test_views.py
+++ test/test_views.py
@@ -10,8 +10,8 @@ class FlaskrTestCase(unittest.TestCase):
 
     def test_outputs(self):
         rv = self.app.get('/outputs')
-        ','.join(SUPPORTED) in rv.data
+        ','.join(SUPPORTED) in str(rv.data)
 
     def test_msg_with_output(self):
         rv = self.app.get('/?output=json')
-        self.assertEquals('{ "imie":"Dawid", "mgs":"Hello World!"}', rv.data)
+        self.assertEquals('{ "imie":"Dawid", "mgs":"Hello World!"}', rv.data.decode('utf-8'))
